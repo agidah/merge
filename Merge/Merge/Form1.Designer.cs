acdaefd5f78d9585d0257b3defb71a93eee6e71e
Noah <celestiarune@icloud.com>
2024-07-26 18:24:16 +0900

    Create merge machine

---
+++ Merge/Merge/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ExcelFileRenamer
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button btnRenameFiles;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnSelectFolderForMerge;
+        private System.Windows.Forms.ListBox listBoxMergeFiles;
+        private System.Windows.Forms.Button btnMergeFiles;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnSelectReferenceFile;
+        private System.Windows.Forms.TextBox txtReferenceFile;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.btnRenameFiles = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnSelectFolderForMerge = new System.Windows.Forms.Button();
+            this.listBoxMergeFiles = new System.Windows.Forms.ListBox();
+            this.btnMergeFiles = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnSelectReferenceFile = new System.Windows.Forms.Button();
+            this.txtReferenceFile = new System.Windows.Forms.TextBox();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(4, 4);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(150, 30);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "폴더 선택";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 15;
+            this.listBoxFiles.Location = new System.Drawing.Point(6, 35);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(450, 184);
+            this.listBoxFiles.TabIndex = 1;
+            // 
+            // btnRenameFiles
+            // 
+            this.btnRenameFiles.Location = new System.Drawing.Point(6, 225);
+            this.btnRenameFiles.Name = "btnRenameFiles";
+            this.btnRenameFiles.Size = new System.Drawing.Size(150, 30);
+            this.btnRenameFiles.TabIndex = 2;
+            this.btnRenameFiles.Text = "시작";
+            this.btnRenameFiles.UseVisualStyleBackColor = true;
+            this.btnRenameFiles.Click += new System.EventHandler(this.btnRenameFiles_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Location = new System.Drawing.Point(12, 12);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(500, 290);
+            this.tabControl.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnSelectFolder);
+            this.tabPage1.Controls.Add(this.listBoxFiles);
+            this.tabPage1.Controls.Add(this.btnRenameFiles);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(452, 262);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "1단계";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtReferenceFile);
+            this.tabPage2.Controls.Add(this.btnSelectReferenceFile);
+            this.tabPage2.Controls.Add(this.lblPassword);
+            this.tabPage2.Controls.Add(this.txtPassword);
+            this.tabPage2.Controls.Add(this.btnSelectFolderForMerge);
+            this.tabPage2.Controls.Add(this.listBoxMergeFiles);
+            this.tabPage2.Controls.Add(this.btnMergeFiles);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(452, 262);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "2단계";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectFolderForMerge
+            // 
+            this.btnSelectFolderForMerge.Location = new System.Drawing.Point(6, 35);
+            this.btnSelectFolderForMerge.Name = "btnSelectFolderForMerge";
+            this.btnSelectFolderForMerge.Size = new System.Drawing.Size(150, 30);
+            this.btnSelectFolderForMerge.TabIndex = 0;
+            this.btnSelectFolderForMerge.Text = "폴더 선택";
+            this.btnSelectFolderForMerge.UseVisualStyleBackColor = true;
+            this.btnSelectFolderForMerge.Click += new System.EventHandler(this.btnSelectFolderForMerge_Click);
+            // 
+            // listBoxMergeFiles
+            // 
+            this.listBoxMergeFiles.FormattingEnabled = true;
+            this.listBoxMergeFiles.ItemHeight = 15;
+            this.listBoxMergeFiles.Location = new System.Drawing.Point(6, 64);
+            this.listBoxMergeFiles.Name = "listBoxMergeFiles";
+            this.listBoxMergeFiles.Size = new System.Drawing.Size(450, 124);
+            this.listBoxMergeFiles.TabIndex = 1;
+            // 
+            // btnMergeFiles
+            // 
+            this.btnMergeFiles.Location = new System.Drawing.Point(6, 225);
+            this.btnMergeFiles.Name = "btnMergeFiles";
+            this.btnMergeFiles.Size = new System.Drawing.Size(150, 30);
+            this.btnMergeFiles.TabIndex = 2;
+            this.btnMergeFiles.Text = "파일 합치기";
+            this.btnMergeFiles.UseVisualStyleBackColor = true;
+            this.btnMergeFiles.Click += new System.EventHandler(this.btnMergeFiles_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(95, 6);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(150, 23);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(6, 9);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(83, 15);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "엑셀 파일 암호:";
+            // 
+            // btnSelectReferenceFile
+            // 
+            this.btnSelectReferenceFile.Location = new System.Drawing.Point(6, 194);
+            this.btnSelectReferenceFile.Name = "btnSelectReferenceFile";
+            this.btnSelectReferenceFile.Size = new System.Drawing.Size(150, 30);
+            this.btnSelectReferenceFile.TabIndex = 5;
+            this.btnSelectReferenceFile.Text = "참조 파일 선택";
+            this.btnSelectReferenceFile.UseVisualStyleBackColor = true;
+            this.btnSelectReferenceFile.Click += new System.EventHandler(this.btnSelectReferenceFile_Click);
+            // 
+            // txtReferenceFile
+            // 
+            this.txtReferenceFile.Location = new System.Drawing.Point(162, 194);
+            this.txtReferenceFile.Name = "txtReferenceFile";
+            this.txtReferenceFile.Size = new System.Drawing.Size(294, 23);
+            this.txtReferenceFile.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 311);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Form1";
+            this.Text = "혜진_정산업로드파일 만들기";
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Merge/Merge/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace ExcelFileRenamer
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFolderPath;
+        private string selectedMergeFolderPath;
+        private string selectedReferenceFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (var folderBrowser = new FolderBrowserDialog())
+            {
+                if (folderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFolderPath = folderBrowser.SelectedPath;
+                    LoadExcelFiles();
+                }
+            }
+        }
+
+        private void LoadExcelFiles()
+        {
+            listBoxFiles.Items.Clear();
+
+            var files = Directory.GetFiles(selectedFolderPath, "*.xlsx")
+                                 .Select(Path.GetFileName)
+                                 .ToList();
+
+            listBoxFiles.Items.AddRange(files.ToArray());
+        }
+
+        private void btnRenameFiles_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedFolderPath))
+            {
+                MessageBox.Show("폴더를 먼저 선택해주세요.");
+                return;
+            }
+
+            var files = Directory.GetFiles(selectedFolderPath, "*.xlsx");
+
+            foreach (var filePath in files)
+            {
+                var fileName = Path.GetFileName(filePath);
+                var newFileName = RenameFile(fileName);
+
+                if (newFileName != fileName)
+                {
+                    var newFilePath = Path.Combine(selectedFolderPath, newFileName);
+                    File.Move(filePath, newFilePath);
+                }
+            }
+
+            MessageBox.Show("파일명이 성공적으로 변경되었습니다.");
+            LoadExcelFiles();
+        }
+
+        private string RenameFile(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+            var nameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+
+            var newName = nameWithoutExtension.Replace("+", "");
+
+            var indexOfDash = newName.IndexOf('-');
+            if (indexOfDash >= 0)
+            {
+                newName = newName.Substring(0, indexOfDash);
+            }
+
+            return newName.Trim() + extension;
+        }
+
+        private void btnSelectFolderForMerge_Click(object sender, EventArgs e)
+        {
+            using (var folderBrowser = new FolderBrowserDialog())
+            {
+                if (folderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    selectedMergeFolderPath = folderBrowser.SelectedPath;
+                    LoadMergeFiles();
+                }
+            }
+        }
+
+        private void LoadMergeFiles()
+        {
+            listBoxMergeFiles.Items.Clear();
+
+            var files = Directory.GetFiles(selectedMergeFolderPath, "*.xlsx")
+                                 .Select(Path.GetFileName)
+                                 .ToList();
+
+            listBoxMergeFiles.Items.AddRange(files.ToArray());
+        }
+
+        private void btnSelectReferenceFile_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Excel Files|*.xlsx";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedReferenceFilePath = openFileDialog.FileName;
+                    txtReferenceFile.Text = selectedReferenceFilePath;
+                }
+            }
+        }
+
+        private void btnMergeFiles_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedMergeFolderPath))
+            {
+                MessageBox.Show("폴더를 먼저 선택해주세요.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(selectedReferenceFilePath))
+            {
+                MessageBox.Show("참조 파일을 선택해주세요.");
+                return;
+            }
+
+            var password = txtPassword.Text;
+
+            var referenceData = LoadReferenceData(selectedReferenceFilePath);
+            var files = Directory.GetFiles(selectedMergeFolderPath, "*.xlsx");
+            using (var package = new ExcelPackage())
+            {
+                var sheet1 = package.Workbook.Worksheets.Add("Sheet1");
+
+                sheet1.Cells[1, 1].Value = "라이더로그인ID";
+                sheet1.Cells[1, 2].Value = "항목명";
+                sheet1.Cells[1, 3].Value = "설명";
+                sheet1.Cells[1, 4].Value = "금액";
+                sheet1.Cells[1, 5].Value = "적용일자(yyyy-MM-dd)";
+
+                int currentRow = 2;
+
+                foreach (var filePath in files)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        using (var excelPackage = new ExcelPackage(stream, password))
+                        {
+                            var sheet = excelPackage.Workbook.Worksheets[0];
+                            int rowCount = sheet.Dimension.Rows;
+
+                            for (int row = 2; row <= rowCount; row++)
+                            {
+                                var colG = sheet.Cells[row, 7].Text;
+                                if (colG == "달성")
+                                {
+                                    var colB = sheet.Cells[row, 2].Text;
+                                    var colD = sheet.Cells[row, 4].Text;
+                                    var colC = Path.GetFileNameWithoutExtension(filePath).Replace(" ", "");
+                                    sheet1.Cells[currentRow, 1].Value = colB;
+                                    sheet1.Cells[currentRow, 2].Value = colD;
+                                    sheet1.Cells[currentRow, 3].Value = colC;
+
+                                    if (referenceData.ContainsKey(colC))
+                                    {
+                                        sheet1.Cells[currentRow, 4].Value = referenceData[colC].Item1;
+                                        sheet1.Cells[currentRow, 5].Value = referenceData[colC].Item2;
+                                    }
+
+                                    currentRow++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                var sheet2 = package.Workbook.Worksheets.Add("Sheet2");
+
+                for (int col = 1; col <= 5; col++)
+                {
+                    sheet2.Cells[1, col].Value = sheet1.Cells[1, col].Value;
+                }
+
+                for (int row = 2; row < currentRow; row++)
+                {
+                    var colE = sheet1.Cells[row, 5].Text;
+                    var colD = sheet1.Cells[row, 4].Text;
+                    sheet2.Cells[row, 3].Value = $"{colE}_배달미션_{colD}";
+                }
+
+                for (int row = 2; row < currentRow; row++)
+                {
+                    sheet2.Cells[row, 4].Value = sheet1.Cells[row, 2].Value;
+                }
+
+                for (int row = 2; row < currentRow; row++)
+                {
+                    sheet2.Cells[row, 2].Value = "기타-지급성(원천세적용)";
+                }
+
+                DateTime today = DateTime.Today;
+                DateTime wednesday = today.AddDays(DayOfWeek.Wednesday - today.DayOfWeek);
+                if (wednesday < today)
+                    wednesday = wednesday.AddDays(7);
+
+                string formattedWednesday = wednesday.ToString("yyyy-MM-dd");
+
+                for (int row = 2; row < currentRow; row++)
+                {
+                    sheet2.Cells[row, 5].Value = formattedWednesday;
+                }
+
+                // Sheet1의 A열 데이터를 Sheet2의 A열로 복사
+                for (int row = 1; row < currentRow; row++)
+                {
+                    sheet2.Cells[row, 1].Value = sheet1.Cells[row, 1].Value;
+                }
+
+                // Sheet1 삭제
+                package.Workbook.Worksheets.Delete(sheet1);
+
+                // Sheet2의 이름을 Sheet1으로 변경
+                sheet2.Name = "Sheet1";
+
+                // D열 데이터를 숫자로 변환하고 1000단위 콤마 형식 적용
+                for (int row = 2; row <= sheet2.Dimension.End.Row; row++)
+                {
+                    if (decimal.TryParse(sheet2.Cells[row, 4].Text, out decimal value))
+                    {
+                        sheet2.Cells[row, 4].Value = value;
+                        sheet2.Cells[row, 4].Style.Numberformat.Format = "#,##0";
+                    }
+                }
+
+                // 셀 너비 자동 조절
+                sheet2.Cells[sheet2.Dimension.Address].AutoFitColumns();
+
+                var outputFilePath = Path.Combine(selectedMergeFolderPath, "정산 업로드.xlsx");
+                package.SaveAs(new FileInfo(outputFilePath));
+            }
+
+            MessageBox.Show("파일이 성공적으로 합쳐졌습니다.");
+        }
+
+        private Dictionary<string, Tuple<string, string>> LoadReferenceData(string referenceFilePath)
+        {
+            var referenceData = new Dictionary<string, Tuple<string, string>>();
+
+            using (var stream = new FileStream(referenceFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var excelPackage = new ExcelPackage(stream))
+                {
+                    var sheet = excelPackage.Workbook.Worksheets[0];
+                    int rowCount = sheet.Dimension.Rows;
+
+                    for (int row = 1; row <= rowCount; row++)
+                    {
+                        var key = sheet.Cells[row, 1].Text.Replace(" ", "");
+                        var value1 = sheet.Cells[row, 2].Text;
+                        var value2 = sheet.Cells[row, 3].Text;
+                        if (!string.IsNullOrEmpty(key) && !referenceData.ContainsKey(key))
+                        {
+                            referenceData.Add(key, new Tuple<string, string>(value1, value2));
+                        }
+                    }
+                }
+            }
+
+            return referenceData;
+        }
+    }
+}
